9d4761c9862d9405ef7a319fb9a82f8540a99d15
kangaroo19 <1000jjj@naver.com>
2024-11-11 23:13:29 +0900

    [FEAT] 스킬카드 컴포넌트 작업중

--- src/components/SkillCard.tsx
+++ src/components/SkillCard.tsx
@@ -1,16 +1,23 @@
 import Image from "next/image";
-import { Progress } from "./ui/progress";
+import { Card, Progress } from "./ui";
 
 export default function SkillCard() {
   return (
-    <div>
+    <Card.Card className="flex w-full items-center space-x-4 p-2 rounded-lg">
+      <span className="text-3xl font-spoqa-regular">1</span>
       <Image
         src="https://cdn.simpleicons.org/react"
         width={50}
         height={50}
         alt="React Icon"
       />
-      <Progress value={23} className="h-4" />
-    </div>
+      <Card.CardHeader className="flex-1 p-2">
+        <Card.CardTitle className="text-lg">리액트</Card.CardTitle>
+        <Progress value={23} className="h-4" />
+      </Card.CardHeader>
+      <div className="flex items-end h-full">
+        <span className="leading-none text-lg">23%</span>
+      </div>
+    </Card.Card>
   );
 }
